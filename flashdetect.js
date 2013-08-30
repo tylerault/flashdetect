eed957f5ca09b4dcfb22590425b3183435d6336a
Tyler Ault <tyler@tylerault.com>
2013-08-29 23:52:37 -0400

    Reduced sizes even further, added tests

--- flashdetect.js
+++ flashdetect.js
@@ -5,38 +5,33 @@
 var getFlashVersion = function() {
 	
 		var SW = "Shockwave",
-			SHOCKWAVE_FLASH = SW + " Flash",
-			SHOCKWAVE_FLASH_AX = SW+"Flash."+SW+"Flash",
+			FLASH = "Flash",
+			SHOCKWAVE_FLASH = SW + " " + FLASH,
+			SHOCKWAVE_FLASH_AX = SW+FLASH+"."+SW+FLASH,
 			FLASH_MIME_TYPE = "application/x-shockwave-flash",
-			undef = function(n){return (typeof n == 'undefined');},
 			nav = navigator,
-			parse = parseInt,
+			parse = parseFloat,
 			plugins = nav.plugins,
 			mimes = nav.mimeTypes,
-			playerVersion = [0,0,0],
-			d = null, a;
-		if (!undef(plugins) && typeof plugins[SHOCKWAVE_FLASH] == "object") {
+			d, a;
+		if (!!plugins && typeof plugins[SHOCKWAVE_FLASH] == "object") {
 			d = plugins[SHOCKWAVE_FLASH].description;
-			if (d && !(!undef(mimes) && mimes[FLASH_MIME_TYPE] && !mimes[FLASH_MIME_TYPE].enabledPlugin)) {
-				d = d.replace(/^.*\s+(\S+\s+\S+$)/, "$1");
-				playerVersion[0] = parse(d.replace(/^(.*)\..*$/, "$1"));
-				playerVersion[1] = parse(d.replace(/^.*\.(.*)\s.*$/, "$1"));
+			if (!!d && !(!!mimes && !!mimes[FLASH_MIME_TYPE] && !mimes[FLASH_MIME_TYPE].enabledPlugin)) {
+				return parse( d.replace(/^.*\s+(\S+)\s+\S+$/, "$1") );
 			}
 		}
-		else if ( !undef(window.ActiveXObject) ) {
+		else if ( !!window.ActiveXObject ) {
 			try {
 				a = new ActiveXObject(SHOCKWAVE_FLASH_AX);
 				if (a) {
 					d = a.GetVariable("$version");
-					if (d) {
-						d = d.split(" ")[1].split(",");
-						playerVersion = [parse(d[0]), parse(d[1])];
+					if (!!d) {
+						return parse(d.replace(/^\S+\s+(\d+),(\d+).*$/i,'$1.$2'));
 					}
 				}
 			}
 			catch(e) {}
 		}
-		a = playerVersion[0];
-		return ( a > 0 ? parseFloat(a+'.'+playerVersion[1]) : -1 );
+		return -1;
 		
 };
